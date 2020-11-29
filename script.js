605e6050696cdbb7c96f787d49886ac4bac57177
ArtART241 <graubergerartem@gmail.com>
2020-11-29 12:48:18 +0100

    adding modal-pop-up-thing

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-
-
-
 $(document).ready(function () {
     //activating tooltip of element
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
+
+    // Var for saving the answer
+    let ans = document.createElement("div");
+
+    $(".modal-body").load("https://artart241.github.io img.d-block.w-100", function (response, status, request) {
+        
+        
+    });
 });
+
+
