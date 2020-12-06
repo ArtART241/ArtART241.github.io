c0ca10bf7336ed772116b326439215ffe9b6eda8
ArtART241 <graubergerartem@gmail.com>
2020-12-06 13:01:25 +0100

    final website

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ $(document).ready(function () {
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
 
-    // Var for saving the answer
-    let ans = document.createElement("div");
-
+   
 });
 
 $("#btnSelect").click(function (e) { 
@@ -13,8 +11,14 @@ $("#btnSelect").click(function (e) {
     let select = document.querySelector("select.custom-select")
     let url = select.options[select.selectedIndex].value + " div#answer";
 
+
+    let selectedUser = select.options[select.selectedIndex].innerHTML;
+
+    document.querySelector("#msgLabel").innerHTML = "Message from user " + "<a href=" + url + "' target='_blank'>" + selectedUser + "</a>";
+
     $(".modal-body").load(url, function (response, status, request) {
         $(".modal-body>div#answer").attr("id", "");
+        $("#msgBox").modal("show");
     });
 
 });
